8d22552630be742586580f063eab4d8b1fc31874
venkatesh kotoju <kotoju.venkatesh@gmail.com>
2025-07-05 18:51:44 +0530

    Fix: disable TypeScript and ESLint rules for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
-import type { NextConfig } from 'next'
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
